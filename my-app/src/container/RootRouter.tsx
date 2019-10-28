1c43bd349426c7359f38439e7af2f2c5e3139111
YifeiLi <liy12@rose-hulman.edu>
2019-10-28 02:51:31 -0400

    add fab

---
+++ my-app/src/container/RootRouter.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+class RootRouter extends React.Component{
+    render(){
+        return <BrowserRouter>
+        </BrowserRouter>
+    }
+}
