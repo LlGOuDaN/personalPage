1817b1a750fc7bbb601fea6e5d66fa3da744c255
YifeiLi <liy12@rose-hulman.edu>
2020-03-10 07:54:34 -0400

    modify top section

--- my-app/src/containers/Main/index.tsx
+++ my-app/src/containers/Main/index.tsx
@@ -8,7 +8,20 @@ export default (props: IMainProps) => {
   return (
     <div className="main-container">
       <NavigationBar />
-      <section className="main-container__top-section"></section>
+      <section className="main-container__top-section">
+        <div className="top-section__intro">
+          <h5>Hi, I'm</h5>
+          <h2>
+            <span className="theme-green">Y</span>ifei{" "}
+            <span className="theme-green">L</span>i
+          </h2>
+          <p className="theme-green">SWE & Front-End Developer</p>
+          <div className="bottom">
+            About Me
+            <span className="about-icon">&#x269B;</span>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
